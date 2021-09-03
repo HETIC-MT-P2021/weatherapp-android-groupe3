a9314edad93ecfc9e5d8888bc8167e30172ca6f3
acauchois <cauchoisalexis@gmail.com>
2021-09-03 18:49:33 +0200

    Improvement of the home page

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.weatherapp
 
+import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Geocoder
@@ -8,13 +9,13 @@ import android.location.LocationManager
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Looper
 import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.example.weatherapp.weather.WeatherResponse
 import com.example.weatherapp.weather.WeatherService
@@ -28,7 +29,9 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.text.NumberFormat
 import java.util.*
+import kotlin.math.round
 
 class MainActivity : AppCompatActivity() {
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
@@ -36,20 +39,39 @@ class MainActivity : AppCompatActivity() {
     private var permissionId = 1234
     private lateinit var locationTxt: TextView
     private lateinit var countryName: String
-    private lateinit var cityName: String
-    private var weatherData: TextView? = null
+    private var cityName: String? = null
+    private var updatedAt: TextView? = null
+    private lateinit var statusTxt: TextView
+    private lateinit var temperatureTxt: TextView
+    private lateinit var tempMinTxt: TextView
+    private lateinit var tempMaxTxt: TextView
+    private lateinit var windTxt: TextView
+    private lateinit var pressureTxt: TextView
+    private lateinit var humidityTxt: TextView
 
-    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        weatherData = findViewById(R.id.currentWeatherTxt)
-        locationTxt = findViewById(R.id.locationTxt)
+        locationTxt = findViewById(R.id.location_txt)
+        statusTxt = findViewById(R.id.status_txt)
+        temperatureTxt = findViewById(R.id.temperature_txt)
+        tempMinTxt = findViewById(R.id.temp_min_txt)
+        tempMaxTxt = findViewById(R.id.temp_max_txt)
+        windTxt = findViewById(R.id.wind_txt)
+        pressureTxt = findViewById(R.id.pressure_txt)
+        humidityTxt = findViewById(R.id.humidity_txt)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onResume() {
+        super.onResume()
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
         if (isNetworkAvailable(applicationContext)) {
             GlobalScope.launch {
                 getLastLocation()
+                delay(200)
+                cityName?.let { getCurrentData(it) }
             }
         }
     }
@@ -66,10 +88,12 @@ class MainActivity : AppCompatActivity() {
             if (isLocationEnabled()) {
                 fusedLocationProviderClient.lastLocation.addOnCompleteListener { task ->
                     displayLocation(task)
-                    getCurrentData()
+//                    cityName?.let {
+//                        getCurrentData(it)
+//                    }
                 }
             } else {
-                Toast.makeText(this, "Please enable your location service", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Veuillez activer le service de géolocalisation", Toast.LENGTH_SHORT).show()
             }
         } else {
             requestPermission()
@@ -136,7 +160,7 @@ class MainActivity : AppCompatActivity() {
         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
     }
 
-    private fun getCityName(lat:Double, long:Double):String {
+    private fun getCityName(lat:Double, long:Double):String? {
         var geoCoder = Geocoder(this, Locale.getDefault())
         var address = geoCoder.getFromLocation(lat, long, 1)
         cityName = address[0].locality
@@ -153,44 +177,34 @@ class MainActivity : AppCompatActivity() {
     companion object {
         var BaseUrl = "https://api.openweathermap.org/"
         var AppId = "f3aa9809cf0afaa4155a1dfe26f5d838"
+        var units = "metric"
         var lang = "fr"
     }
 
-    private fun getCurrentData() {
+    private fun getCurrentData(city: String) {
         val retrofit = Retrofit.Builder()
             .baseUrl(BaseUrl)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
         val service = retrofit.create(WeatherService::class.java)
-        val call = service.getCurrentWeatherData(cityName, AppId, lang)
+        val call = service.getCurrentWeatherData(city, AppId, lang, units)
         call.enqueue(object : Callback<WeatherResponse> {
             override fun onResponse(call: Call<WeatherResponse>, response: Response<WeatherResponse>) {
                 if (response.code() == 200) {
                     val weatherResponse = response.body()!!
 
-                    val stringBuilder = weatherResponse.weather[0].description +
-                            "\n" +
-                            "Température: " +
-                            weatherResponse.main!!.temp +
-                            "\n" +
-                            "Température(Min): " +
-                            weatherResponse.main!!.temp_min +
-                            "\n" +
-                            "Température(Max): " +
-                            weatherResponse.main!!.temp_max +
-                            "\n" +
-                            "Humidité: " +
-                            weatherResponse.main!!.humidity +
-                            "\n" +
-                            "Pression: " +
-                            weatherResponse.main!!.pressure
-
-                    weatherData!!.text = stringBuilder
+                    statusTxt.text = weatherResponse.weather[0].description
+                    temperatureTxt.text = NumberFormat.getInstance().format(round(weatherResponse.main!!.temp)).toString() + "°C"
+                    tempMinTxt.text = NumberFormat.getInstance().format(round(weatherResponse.main!!.temp_min)).toString() + "°C"
+                    tempMaxTxt.text = NumberFormat.getInstance().format(round(weatherResponse.main!!.temp_max)).toString() + "°C"
+                    windTxt.text = weatherResponse.wind?.speed?.let { round(it).toString() }
+                    pressureTxt.text = NumberFormat.getInstance().format(round(weatherResponse.main!!.pressure)).toString()
+                    humidityTxt.text = NumberFormat.getInstance().format(round(weatherResponse.main!!.humidity)).toString()
                 }
             }
 
             override fun onFailure(call: Call<WeatherResponse>, t: Throwable) {
-                weatherData!!.text = t.message
+                updatedAt!!.text = t.message
             }
         })
     }
@@ -203,7 +217,7 @@ class MainActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == permissionId) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Log.d("Debug: ", "Permission granted")
+                Log.d("Debug: ", "Permission accordée")
             }
         }
     }
--- app/src/main/java/com/example/weatherapp/weather/WeatherService.kt
+++ app/src/main/java/com/example/weatherapp/weather/WeatherService.kt
@@ -7,5 +7,5 @@ import retrofit2.http.Query
 
 interface WeatherService {
     @GET("data/2.5/weather?")
-    fun getCurrentWeatherData(@Query("q") q: String, @Query("APPID") app_id: String, @Query("lang") lang: String): Call<WeatherResponse>
+    fun getCurrentWeatherData(@Query("q") q: String, @Query("APPID") app_id: String, @Query("lang") lang: String, @Query("units") units: String): Call<WeatherResponse>
 }
